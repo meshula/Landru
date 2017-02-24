bd6f559fd2dc5c675cc33c212c060b78cd1fc8df
Nick Porcino <meshula@hotmail.com>
2017-02-23 22:19:53 -0800

    update render build

--- thirdparty/LabRender.cmd
+++ thirdparty/LabRender.cmd
@@ -22,3 +22,11 @@ msbuild ..\..\prereq\LabRender\LabRender.sln /t:Build /p:Configuration=Release /
 msbuild ..\..\prereq\LabRender\LabRender.sln /t:Build /p:Configuration=Debug /p:Platform=windows
 
 cd %current%
+
+xcopy .\prereq\LabRender\include\*.h .\include\ /s /y
+copy .\prereq\LabRender\bin\windows\Debug\labrender.* .\lib\windows\Debug\
+copy .\prereq\LabRender\bin\windows\Release\labrender.* .\lib\windows\Release\
+copy .\prereq\LabRender\bin\windows\Debug\*.dll ..\bin\windows\Debug\
+copy .\prereq\LabRender\bin\windows\Debug\*.exe ..\bin\windows\Debug\
+copy .\prereq\LabRender\bin\windows\Release\*.dll ..\bin\windows\Release\
+copy .\prereq\LabRender\bin\windows\Release\*.exe ..\bin\windows\Release\
