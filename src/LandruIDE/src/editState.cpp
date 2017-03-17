8af403619838e036b1339938d1037806492b812e
Nick Porcino <meshula@hotmail.com>
2017-03-16 23:34:35 -0700

    Add stage layer creation

--- src/LandruIDE/src/editState.cpp
+++ src/LandruIDE/src/editState.cpp
@@ -1,15 +1,41 @@
 
 #include "editState.h"
+#include "interface/boxer.h"
+#include <imgui.h>
 #include <LabRender/Camera.h>
+#include <ctime>
+
+#ifdef _MSC_VER
+// suppress warnings until USD is cleaned up
+#  pragma warning(push)
+#  pragma warning(disable : 4244 4305)
+#endif
+
 #include <pxr/usd/usd/stage.h>
 #include <pxr/base/arch/nap.h>
 
+#ifdef _MSC_VER
+// suppress warnings until USD is cleaned up
+#  pragma warning(pop)
+#endif
+
 namespace lab {
 
+	// camera
 	event<void(float deltax, float deltay)> evt_camera_mouse;
     event<void(std::shared_ptr<Camera>)> evt_bind_view_camera;
 	event<void(CameraRig::Mode)> evt_set_camera_mode;
+
+	// manipulator
 	event<void(EditState::ManipulatorMode)> evt_set_manipulator_mode;
+	event<void(const EditState::float16 &)> evt_set_manipulated_matrix;
+
+	// stage
+	event<void()> evt_new_stage;
+	event<void(UsdStageRefPtr new_stage)> evt_stage_created;
+	event<void(UsdStageRefPtr new_stage)> evt_stage_closing;
+	event<void(const std::string&)> evt_new_layer;
+	event<void(const std::string&)> evt_layer_created;
 
 
 struct EditState::_Detail
@@ -20,6 +46,10 @@ struct EditState::_Detail
         evt_bind_view_camera.connect(this, &_Detail::bind_view_camera);
 		evt_set_camera_mode.connect(this, &_Detail::set_camera_mode);
 		evt_set_manipulator_mode.connect(this, &_Detail::set_manipulator_mode);
+		evt_set_manipulated_matrix.connect(this, &_Detail::set_manipulated_matrix);
+		evt_new_stage.connect(this, &_Detail::new_stage);
+		evt_new_layer.connect(this, &_Detail::new_layer);
+
     }
     ~_Detail()
     {
@@ -27,11 +57,19 @@ struct EditState::_Detail
 		evt_bind_view_camera.disconnect(this, &_Detail::bind_view_camera);
 		evt_set_camera_mode.disconnect(this, &_Detail::set_camera_mode);
 		evt_set_manipulator_mode.disconnect(this, &_Detail::set_manipulator_mode);
+		evt_set_manipulated_matrix.disconnect(this, &_Detail::set_manipulated_matrix);
+		evt_new_stage.connect(this, &_Detail::new_stage);
+		evt_new_layer.disconnect(this, &_Detail::new_layer);
 	}
 
 	void set_manipulator_mode(ManipulatorMode m)
 	{
 		mode = m;
+	}
+
+	void set_manipulated_matrix(const EditState::float16 & m)
+	{
+		memcpy(&manipulator_matrix, &m, sizeof(EditState::float16));
 	}
 
 	void set_camera_mode(CameraRig::Mode mode)
@@ -50,9 +88,44 @@ struct EditState::_Detail
         _currentCamera = camera;
     }
 
+	void new_stage()
+	{
+		if (_stage)
+		{
+			boxer::Selection choice = boxer::show("Create new stage?", "Closing stage", boxer::Style::Warning, boxer::Buttons::OKCancel);
+			if (choice == boxer::Selection::Cancel)
+				return;
+
+			evt_stage_closing(_stage);
+			_stage->Close();
+		}
+
+		std::time_t now = std::time(nullptr);
+		std::tm * ltime = localtime(&now);
+		char tbuff[80];
+		sprintf(tbuff, "session_%04d%02d%02d_%02d%02d.usda", ltime->tm_year + 1900, ltime->tm_mon + 1, ltime->tm_mday,
+			ltime->tm_hour, ltime->tm_min);
+
+		_stage = UsdStage::CreateNew(tbuff);
+		evt_stage_created(_stage);
+	}
+
+	void new_layer(const std::string & layer)
+	{
+		if (!_stage)
+			return;
+
+		auto root_layer = _stage->GetRootLayer();
+		auto new_layer = SdfLayer::CreateNew(layer);
+		root_layer->GetSubLayerPaths().push_back(layer);
+		evt_layer_created(layer);
+	}
+
     std::shared_ptr<Camera> _currentCamera;
     CameraRig rig;
 	ManipulatorMode mode = ManipulatorMode::Translate;
+
+	float manipulator_matrix[16] = { 1,0,0,0, 0,1,0,0, 0,0,1,0, 0,0,0,1 };
 
 	UsdStageRefPtr _stage;
 };
@@ -60,12 +133,6 @@ struct EditState::_Detail
 EditState::EditState()
 : _detail(new _Detail())
 {
-	_detail->_stage = UsdStage::Open("C:/Projects/assets/k142/MrRayLayers.usd");
-	if (!_detail->_stage)
-	{
-		ArchSleep(1);
-		_detail->_stage = UsdStage::Open("C:/Projects/assets/k142/MrRayLayers.usd");
-	}
 }
 
 EditState::~EditState()
@@ -82,6 +149,12 @@ EditState::ManipulatorMode EditState::manipulator_mode() const
 {
 	return _detail->mode;
 }
+
+const EditState::float16 & EditState::manipulated_matrix() const
+{
+	return _detail->manipulator_matrix;
+}
+
 
 } // lab
 
--- src/LandruIDE/src/editState.h
+++ src/LandruIDE/src/editState.h
@@ -5,7 +5,20 @@
 #include <LabRender/Camera.h>
 #include <memory>
 
+
+#ifdef _MSC_VER
+// suppress warnings until USD is cleaned up
+#  pragma warning(push)
+#  pragma warning(disable : 4244 4305)
+#endif
+
 #include <pxr/usd/usd/stage.h>
+
+
+#ifdef _MSC_VER
+// suppress warnings until USD is cleaned up
+#  pragma warning(pop)
+#endif
 
 namespace lab {
 
@@ -26,12 +39,25 @@ namespace lab {
 		ManipulatorMode manipulator_mode() const;
 
 		UsdStageRefPtr stage();
+
+		typedef float float16[16];
+		const float16 & manipulated_matrix() const;
     };
 
+	// camera
     extern event<void(float deltax, float deltay)> evt_camera_mouse;
     extern event<void(std::shared_ptr<Camera>)> evt_bind_view_camera;
 	extern event<void(CameraRig::Mode)> evt_set_camera_mode;
 
+	// manipulator
 	extern event<void(EditState::ManipulatorMode)> evt_set_manipulator_mode;
+	extern event<void(const EditState::float16 &)> evt_set_manipulated_matrix;
+
+	// stage
+	extern event<void()> evt_new_stage;
+	extern event<void(UsdStageRefPtr)> evt_stage_created;
+	extern event<void(UsdStageRefPtr)> evt_stage_closing;
+	extern event<void(const std::string&)> evt_new_layer;
+	extern event<void(const std::string&)> evt_layer_created;
 
 } // lab
