9178e0b14c8796075b94191a193c188219ec5f13
Nick Porcino <meshula@hotmail.com>
2017-03-18 13:01:49 -0700

    Add major and minor modes

---
+++ src/LandruIDE/src/modes.cpp
@@ -0,0 +1,22 @@
+
+#include "modes.h"
+
+namespace lab
+{
+	void ModeManager::add_mode(std::shared_ptr<MinorMode> m)
+    {
+        _minorModes[m->name()] = m;
+    }
+
+	void ModeManager::add_mode(std::shared_ptr<MajorMode> m)
+    {
+        _majorModes[m->name()] = m;
+    }
+
+	void ModeManager::update()
+	{
+		for (auto i : _minorModes)
+			i.second->update();
+	}
+
+}
---
+++ src/LandruIDE/src/modes.h
@@ -0,0 +1,48 @@
+
+#pragma once
+
+#include "editState.h"
+#include "interface/labGraphicsWindow.h"
+
+#include <map>
+#include <string>
+
+namespace lab
+{
+
+	class MajorMode
+	{
+	public:
+		virtual ~MajorMode() {}
+		virtual void ui(EditState & es, GraphicsWindowManager & mgr, ImGuiDock::Dockspace & dockspace) = 0;
+		virtual const char * name() const = 0;
+	};
+
+	class MinorMode
+	{
+	public:
+		virtual ~MinorMode() {}
+		virtual const char * name() const = 0;
+		virtual bool is_singleton() const { return true; }
+
+		virtual void ui(lab::EditState& edit_state,
+			lab::CursorManager& cursorManager,
+			lab::FontManager& fontManager,
+			float width, float height) {}
+
+		virtual void update() {}
+	};
+
+	class ModeManager
+	{
+		std::map < std::string, std::shared_ptr<MinorMode> > _minorModes;
+		std::map < std::string, std::shared_ptr<MajorMode> > _majorModes;
+
+	public:
+		void add_mode(std::shared_ptr<MinorMode>);
+		void add_mode(std::shared_ptr<MajorMode>);
+
+		void update();
+	};
+
+}
